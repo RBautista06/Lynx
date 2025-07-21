0363d88d4e18d8b41b6a5b2c96597679d444b53f
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-07-21 20:30:54 +0800

    asd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,29 @@
-import HeroSection from "./component/HeroSection";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
 
 const App = () => {
   return (
-    <div>
-      <SignupPage />
-      <HeroSection/>
+    <div className="flex items-center justify-center">
+      <div className="border h-screen w-6xl">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 };
--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,5 +1,15 @@
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <p className="font-['GeneralSans-Regular'] text-lg">
+        This uses GeneralSans-Regular
+      </p>
+
+      <h1 className="font-['GeneralSans-Variable'] font-bold text-4xl">
+        Variable font in action
+      </h1>
+    </div>
+  );
 };
 
 export default Homepage;
