3a3b5302471147278acefbb30d2edde6313675a6
Railley Nickolei Vince Bautisa <112752968+RBautista06@users.noreply.github.com>
2025-07-29 20:32:34 +0800

    Update PostFeed.tsx

--- client/src/component/PostFeed.tsx
+++ client/src/component/PostFeed.tsx
@@ -1,7 +1,7 @@
 const PostFeed = () => {
   return (
     <div className=" h-full w-7xl flex justify-center">
-      <div className=" w-4xl bg-gray-900">News Feed</div>
+      <div className=" w-4xl bg-gray-900">News sFeed</div>
     </div>
   );
 };
