d54465735d76820a0bbf864c6c5352b01ee32280
Railley Nickolei Vince Bautisa <112752968+RBautista06@users.noreply.github.com>
2025-08-24 20:52:04 +0800

    Update ProfilePage.tsx

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -55,7 +55,7 @@ const ProfilePage = () => {
                     </div>
                   </div>
                 </div>
-                {/* user postsss */}
+                {/* user postsssss */}
               </div>
               <div className="border h-100">posts here</div>
             </div>
