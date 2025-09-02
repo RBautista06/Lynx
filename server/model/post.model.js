63515eff948954a9ac06ae580c623d01c722a66e
Railley Nickolei Vince Bautisa <railleynickoleivincebautista@gmail.com>
2025-09-02 15:10:34 +0800

    poost schema

---
+++ server/model/post.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const postSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    caption: {
+      type: String,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+      },
+    ],
+    comments: [commentSchema],
+    media: [
+      {
+        type: String,
+      },
+    ],
+    visibility: {
+      type: String,
+      enum: ["public", "followers", "private"],
+      //In Mongoose, enum lets you restrict a field’s values to a specific set of options (like a dropdown). If you try to save something outside the allowed values, MongoDB will reject it.
+      default: "public",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Post = mongoose.model("Post", postSchema);
