8e2361d1a40dec52b46aeeb7b528e4bb18c18714
Railley Nickolei Vince Bautisa <112752968+RBautista06@users.noreply.github.com>
2025-09-15 21:08:39 +0800

    Update ProfilePage.tsx

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -35,7 +35,7 @@ const ProfilePage = () => {
                     </div>
 
                     <div className="flex gap-10">
-                      {/* number of postssssss */}
+                      {/* number of postsssssss */}
                       <div className="flex gap-2">
                         <span className="font-semibold">3</span>{" "}
                         <p className="opacity-50">posts</p>
